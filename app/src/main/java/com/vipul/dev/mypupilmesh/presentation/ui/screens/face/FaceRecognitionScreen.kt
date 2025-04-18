cd762574e717918222702be828e17a0066e6ae2d
Vipul Choudhary <vipulchoudhary566@gmail.com>
2025-04-18 04:14:31 +0000

    Open camera preview on click of face navigation item

--- app/src/main/java/com/vipul/dev/mypupilmesh/presentation/ui/screens/face/FaceRecognitionScreen.kt
+++ app/src/main/java/com/vipul/dev/mypupilmesh/presentation/ui/screens/face/FaceRecognitionScreen.kt
@@ -1,19 +1,13 @@
 package com.vipul.dev.mypupilmesh.presentation.ui.screens.face
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import com.vipul.dev.mypupilmesh.presentation.ui.screens.face.camera.CameraPermissionWrapper
+import com.vipul.dev.mypupilmesh.presentation.ui.screens.face.camera.CameraScreen
 
 @Composable
 fun FaceRecognitionScreen(modifier: Modifier = Modifier) {
-
-
-    Box(modifier= Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Text("Face Recognition", fontSize = 24.sp)
+    CameraPermissionWrapper {
+        CameraScreen()
     }
-
 }
